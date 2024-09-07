4b5755467dc1ae0191696a4c0880a7ca09fc00ab
ccmmasi <ccmmasi@hotmail.com>
2024-09-07 18:42:18 -0500

    migrations

--- Beneficiaries.Solution/Beneficiaries.Core/Migrations/20240907234202_InitialMigration.Designer.cs
+++ Beneficiaries.Solution/Beneficiaries.Core/Migrations/20240907234202_InitialMigration.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Beneficiaries.Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240907010147_InitialMigration")]
+    [Migration("20240907234202_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
